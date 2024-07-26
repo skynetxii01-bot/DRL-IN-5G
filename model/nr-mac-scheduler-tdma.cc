dd202201464baadb003ac5df8de6524edd891f4a
Hyerin Kim <mye280c37@gmail.com>
2024-07-26 14:33:16 +0900

    nr-mac-scheduler-tdma: Apply updated flags

--- model/nr-mac-scheduler-tdma.cc
+++ model/nr-mac-scheduler-tdma.cc
@@ -140,7 +140,7 @@ NrMacSchedulerTdma::AssignRBGTDMA(uint32_t symAvail,
 
     while (resources > 0)
     {
-        if (m_activeAIDL || m_activeAIUL)
+        if (m_activeDlAi || m_activeUlAi)
         {
             callNotifyFn(ueVector);
         }
