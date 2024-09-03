43ff8b1f628c035445770a7ac40ada879f036141
Hyerin Kim <mye280c37@gmail.com>
2024-09-04 03:36:20 +0900

    nr-mac-scheduler-tdma-ai: Separate the accessor for the active flag from the notify callback

--- model/nr-mac-scheduler-tdma-ai.cc
+++ model/nr-mac-scheduler-tdma-ai.cc
@@ -6,6 +6,7 @@
 
 #include "nr-mac-scheduler-tdma-ai.h"
 
+#include <ns3/boolean.h>
 #include <ns3/callback.h>
 #include <ns3/log.h>
 
@@ -28,13 +29,23 @@ NrMacSchedulerTdmaAi::GetTypeId()
             .AddAttribute("NotifyCbDl",
                           "The callback function to notify the AI model for the downlink",
                           CallbackValue(MakeNullCallback<NrMacSchedulerUeInfoAi::NotifyCb>()),
-                          MakeCallbackAccessor(&NrMacSchedulerTdmaAi::SetNotifyCbDl),
+                          MakeCallbackAccessor(&NrMacSchedulerTdmaAi::m_notifyCbDl),
                           MakeCallbackChecker())
             .AddAttribute("NotifyCbUl",
                           "The callback function to notify the AI model for the uplink",
                           CallbackValue(MakeNullCallback<NrMacSchedulerUeInfoAi::NotifyCb>()),
-                          MakeCallbackAccessor(&NrMacSchedulerTdmaAi::SetNotifyCbUl),
-                          MakeCallbackChecker());
+                          MakeCallbackAccessor(&NrMacSchedulerTdmaAi::m_notifyCbUl),
+                          MakeCallbackChecker())
+            .AddAttribute("ActiveDlAi",
+                          "The flag to activate the AI model for the downlink",
+                          BooleanValue(false),
+                          MakeBooleanAccessor(&NrMacSchedulerTdmaAi::m_activeDlAi),
+                          MakeBooleanChecker())
+            .AddAttribute("ActiveUlAi",
+                          "The flag to activate the AI model for the uplink",
+                          BooleanValue(false),
+                          MakeBooleanAccessor(&NrMacSchedulerTdmaAi::m_activeUlAi),
+                          MakeBooleanChecker());
     return tid;
 }
 
