d946c1359210949e5f3fbf7e5cb5d35301aa1af4
Hyerin Kim <mye280c37@gmail.com>
2024-09-10 16:26:30 +0900

    test-ppo: Update variable names

--- examples/cttc-nr-rl-based-sched/test-ppo.py
+++ examples/cttc-nr-rl-based-sched/test-ppo.py
@@ -25,18 +25,18 @@ class PPO:
         lr=0.0003,
         gamma=0.99,
         eps_clip=0.2,
-        K_epochs=4,
+        k_epochs=4,
     ):
         self.gamma = gamma
         self.eps_clip = eps_clip
-        self.K_epochs = K_epochs
+        self.k_epochs = k_epochs
 
         self.policy = self.ActorCritic(state_shape, action_dim, hidden_dim)
         self.optimizer = optim.Adam(self.policy.parameters(), lr=lr)
         self.policy_old = self.ActorCritic(state_shape, action_dim, hidden_dim)
         self.policy_old.load_state_dict(self.policy.state_dict())
 
-        self.MseLoss = nn.MSELoss()
+        self.mse_loss = nn.MSELoss()
 
     class ActorCritic(nn.Module):
         def __init__(self, state_shape, action_dim, hidden_dim):
@@ -91,7 +91,7 @@ class PPO:
             )  # Stack results into a (1000, action_dim) tensor
             dist = Categorical(action_probs)
 
-            action_logprobs = dist.log_prob(action)
+            action_log_probs = dist.log_prob(action)
             dist_entropy = dist.entropy()
 
             # Flatten each state in the batch for the critic
@@ -100,7 +100,7 @@ class PPO:
 
             state_value = self.critic(flattened_state)  # Pass flattened states to the critic
 
-            return action_logprobs, torch.squeeze(state_value), dist_entropy
+            return action_log_probs, torch.squeeze(state_value), dist_entropy
 
     def select_action(self, state, memory):
         action, action_logprob = self.policy_old.act(state)
@@ -132,7 +132,7 @@ class PPO:
         else:
             old_logprobs = torch.tensor(np.array(memory.logprobs), dtype=torch.float32)
 
-        for _ in range(self.K_epochs):
+        for _ in range(self.k_epochs):
             logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
 
             ratios = torch.exp(logprobs - old_logprobs.detach())
@@ -146,7 +146,7 @@ class PPO:
 
             loss = (
                 -torch.min(surr1, surr2)
-                + 0.5 * self.MseLoss(state_values, rewards)
+                + 0.5 * self.mse_loss(state_values, rewards)
                 - 0.01 * dist_entropy
             )
 
