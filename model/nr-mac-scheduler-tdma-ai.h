c52ce98e0e325925433ced0d007d97c8ce19787e
Hyerin Kim <mye280c37@gmail.com>
2024-09-10 17:18:33 +0900

    nr-mac-scheduler-tdma-ai: Update minor details in description

--- model/nr-mac-scheduler-tdma-ai.h
+++ model/nr-mac-scheduler-tdma-ai.h
@@ -20,7 +20,7 @@ namespace ns3
  * This class extends the NrMacSchedulerTdmaQos class and implements the AI
  * scheduler for the downlink and uplink. If the AI model is activated, the scheduler
  * uses the AI model to schedule the UEs. If the AI model is not activated, the scheduler
- * uses the QoS scheduler to schedule the UEs.
+ * works in a similar manner as the QoS scheduler.
  *
  * When the AI model is activated, the scheduler sends observations to the OpenGymEnv
  * class in the ns3-gym module, which are used to train the AI model. The AI model then
@@ -160,7 +160,7 @@ class NrMacSchedulerTdmaAi : public NrMacSchedulerTdmaQos
         const std::vector<NrMacSchedulerNs3::UePtrAndBufferReq>& ueVector) const override;
 
     /**
-     * \brief Update weights of all UE for downlink
+     * \brief Update weights of all UEs for downlink
      * \param ueWeights An unordered map where the key is the UE's RNTI (Radio Network Temporary
      * Identifier) and the value is the UE's weights for all flows
      * \param ueVector A vector
@@ -171,7 +171,7 @@ class NrMacSchedulerTdmaAi : public NrMacSchedulerTdmaQos
         const std::vector<NrMacSchedulerNs3::UePtrAndBufferReq>& ueVector) const;
 
     /**
-     * \brief Update weights of all UE for uplink
+     * \brief Update weights of all UEs for uplink
      * \param ueWeights An unordered map where the key is the UE's RNTI (Radio Network Temporary
      * Identifier) and the value is the UE's weights for all flows
      * \param ueVector A vector
