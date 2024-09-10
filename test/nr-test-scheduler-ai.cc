66b79e163b31f787e013f493761fe4b752eae951
Hyerin Kim <mye280c37@gmail.com>
2024-09-10 15:07:35 +0900

    nr-test-scheduler-ai: Fix minor details in description

--- test/nr-test-scheduler-ai.cc
+++ test/nr-test-scheduler-ai.cc
@@ -31,7 +31,7 @@
  * is created correctly.
  *
  * This unit test is designed to verify the functionality of the callback used for invoking
- * the gym module during the resource assigning process of an AI scheduler.
+ * the ns3-gym module during the resource assigning process of an AI scheduler.
  * The test defines a custom callback and checks whether the information passed as
  * arguments matches the information of the User Equipment (UE) and the associated flow installed in
  * each UE. Specifically, the test involves three UEs, each containing flow information
