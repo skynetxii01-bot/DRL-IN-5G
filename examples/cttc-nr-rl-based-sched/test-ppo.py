55979a02a5ad99a76a63788c99dd433aab2af231
Hyerin Kim <mye280c37@gmail.com>
2024-09-18 20:32:57 +0900

    test-ppo: Update argument details

--- examples/cttc-nr-rl-based-sched/test-ppo.py
+++ examples/cttc-nr-rl-based-sched/test-ppo.py
@@ -286,7 +286,7 @@ def main(args):
         "--schedulerType": "Ai",  # Type of scheduler being used in the simulation (here, AI-based)
     }
     # Create the environment
-    env = ns3env.Ns3Env(port=args.port, simSeed=args.seed, simArgs=simArgs, debug=args.debug)
+    env = ns3env.Ns3Env(port=args.port, simSeed=args.simSeed, simArgs=simArgs, debug=args.debug)
     state_shape = env.observation_space.shape
     action_dim = env.action_space.shape[0]
     ppo = PPO(state_shape, action_dim)
@@ -329,7 +329,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Arguments used for the gym script
     parser.add_argument("--port", type=int, default=5552, help="Port number")
-    parser.add_argument("--seed", type=int, default=3002, help="Seed number")
+    parser.add_argument("--simSeed", type=int, default=3002, help="Seed number")
     parser.add_argument("--debug", type=bool, default=False, help="Debug mode")
     # Arguments used for the ns3 simulation (simArgs)
     parser.add_argument(
@@ -370,12 +370,12 @@ if __name__ == "__main__":
         "--outputDir", type=str, default="./", help="Directory where output data will be stored"
     )
     parser.add_argument(
-        "--enableOfdma", type=bool, default=False, help="Whether to enable OFDMA in the simulation"
+        "--enableOfdma", type=int, default=0, help="Whether to enable OFDMA in the simulation"
     )
     parser.add_argument(
         "--enableQoSLcScheduler",
-        type=bool,
-        default=False,
+        type=int,
+        default=0,
         help="Whether to enable QoS LC scheduler",
     )
     # Update interval for the PPO algorithm
